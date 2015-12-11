8cef7f855d1bceb1169d4799d34e4cc6c769c183
Wieland van Dijk <wvdijk@gmail.com>
2015-12-11 16:26:29 +0100

    Buurman en Buurman

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-var x = ["Yippie-Ki-Yay Motherfucker!", "Qapla'!", "Cthulhu fhtagn!", "GE-WEL-DIG!!1!11!!", "Verhip!", "Jofel!", "Vet!", "Drommels!", "l33t!"];
+var x = ["Yippie-Ki-Yay Motherfucker!", "Qapla'!", "Cthulhu fhtagn!", "GE-WEL-DIG!!1!11!!", "Verhip!", "Jofel!", "Vet!", "Drommels!", "l33t!", "Ajeto!"];
 var y = document.getElementsByClassName("paywall-popup__header")[0].textContent;
 var z = x[Math.floor(Math.random() * x.length)];
 var res = y.replace("Geweldig.", z);
